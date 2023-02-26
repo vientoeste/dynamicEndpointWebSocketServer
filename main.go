d8470c3a0609dc2758da2d3134818b7eed926ed1
este <djjd2000@naver.com>
2023-02-27 04:55:34 +0900

    Style: 2 spaces instead of tab

--- main.go
+++ main.go
@@ -1,107 +1,111 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+  "fmt"
+  "log"
+  "net/http"
 
-	"github.com/gorilla/websocket"
+  "github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
+  ReadBufferSize:  1024,
+  WriteBufferSize: 1024,
 }
 
 // map of dynamic endpoints to handler functions
 var endpoints = make(map[string]func(*websocket.Conn))
 
 func main() {
-	// register endpoint handlers
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/ws", wsHandler)
+  // register endpoint handlers
+  http.HandleFunc("/", homeHandler)
+  http.HandleFunc("/ws", wsHandler)
 
-	fmt.Println("Server started on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+  fmt.Println("Server started on port 8080")
+  log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the home page!")
+  fmt.Println("123")
+  fmt.Fprintf(w, "Welcome to the home page!")
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
-	// HTTP > WebSocket upgrader
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer conn.Close()
+  // HTTP > WebSocket upgrader
+  conn, err := upgrader.Upgrade(w, r, nil)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  defer conn.Close()
 
-	for {
-		_, p, err := conn.ReadMessage()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+  for {
+    _, p, err := conn.ReadMessage()
+    if err != nil {
+      fmt.Println(err)
+      return
+    }
 
-		// Print message to console
-		fmt.Println(string(p))
+    // Print message to console
+    fmt.Println(string(p))
 
-		// Route message to appropriate handler function
-		switch string(p) {
-		case "register":
-			registerEndpoint(conn)
-		default:
-			handleEndpoint(string(p), conn)
-		}
-	}
+    // Route message to appropriate handler function
+    switch string(p) {
+    case "register":
+      registerEndpoint(conn)
+    default:
+      handleEndpoint(string(p), conn)
+    }
+  }
 }
 
 func registerEndpoint(conn *websocket.Conn) {
-	// read new endpoint name from client
-	_, p, err := conn.ReadMessage()
+  // read new endpoint name from client
+  _, p, err := conn.ReadMessage()
 
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
 
-	// Create new handler function for new endpoint
-	endpointName := string(p)
-	endpointFunc := func(conn *websocket.Conn) {
-		for {
-			// Read message from client
-			messageType, p, err := conn.ReadMessage()
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
+  // Create new handler function for new endpoint
+  endpointName := string(p)
+  endpointFunc := func(conn *websocket.Conn) {
+    for {
+      // Read message from client
+      messageType, p, err := conn.ReadMessage()
+      if err != nil {
+        fmt.Println(err)
+        return
+      }
 
-			// Print message to console
-			fmt.Println(string(p))
+      // Print message to console
+      fmt.Println(string(p))
 
-			// Send message back to client
-			err = conn.WriteMessage(messageType, p)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-		}
-	}
+      // Send message back to client
+      err = conn.WriteMessage(messageType, p)
+      if err != nil {
+        fmt.Println(err)
+        return
+      }
+    }
+  }
 
-	// add new endpoint to the map
-	endpoints[endpointName] = endpointFunc
+  // add new endpoint to the map
+  endpoints[endpointName] = endpointFunc
 }
 
 func handleEndpoint(endpoint string, conn *websocket.Conn) {
-	// get handler function for the endpoint from the map
-	endpointFunc, ok := endpoints[endpoint]
-	if !ok {
-		fmt.Println("Invalid endpoint")
-		return
-	}
+  // get handler function for the endpoint from the map
+  endpointFunc, ok := endpoints[endpoint]
+  for key, _ := range endpoints {
+    fmt.Println(key)
+  }
+  if !ok {
+    fmt.Println("Invalid endpoint")
+    return
+  }
 
-	// call handler function for endpoint
-	endpointFunc(conn)
+  // call handler function for endpoint
+  endpointFunc(conn)
 }
